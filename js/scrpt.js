2ac2c142c890a4224114b6cb3ae0ea2576dcdc39
niccoloruberti <nikiruberti@gmail.com>
2023-05-26 17:17:58 +0200

    esercizio base completato

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,1 +1,34 @@
 'use strict';
+
+//dichiaro le variabili
+const grid = document.getElementById('grid');
+const bottone = document.getElementById('generator');
+let cell;
+
+//creo la funzione che mi permette di creare le celle
+function createCell() {
+    let cell = document.createElement('div');
+    //assegno una classe per stilizzare la cella
+    cell.classList.add('cell');
+    return cell;
+}
+
+//creo il ciclo che aggiunge le celle alla griglia quado viene premuto il bottone
+bottone.addEventListener('click', function() {
+    for (let counter = 1; counter <= 100; counter++) {
+        console.log(counter)
+        //creo una nuova cella
+        cell = createCell();
+        //inserisco il testo nella cella
+        cell.innerText = counter;
+        //creo un evento relativo al click sulla singola cella che gli fa cambiare colore
+        cell.addEventListener('click', function() {
+            this.classList.toggle('active');
+            console.log(this.innerText);
+        })
+        //inserisco la nuova cella nella griglia
+        grid.append(cell);
+    }
+})
+
+
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Ogni cella ha un numero progressivo, da 1 a 100. Ci saranno quindi 10 caselle pe
 - creo un bottone che permette all'utente di generare la griglia;
 - creo un contenitore nel DOM che andrà a contenere la griglia (div);
 - creo una funzione che mi crei un nuvo elemento div che saranno le celle di cui verrà composta la griglia;
-- creo un ciclo di 100 iterazioni (numero di celle da creare);
+- creo un ciclo di 100 iterazioni (numero di celle da creare) con eventlistener relativo al bottone che permette all'utente di generare la griglia;
 - ad ogni ciclata utilizzando la funzione che mi genera una nuova cella la vado ad aggiungere al contenitore della griglia;
 - creo un eventlistener di tipo click relativo alla cella che viene clickata (this);
 - aggiungo una classe che fa cambiare colore alla cella quando viene clickata;
