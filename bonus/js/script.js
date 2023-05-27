f4ad18968bbd83a1310465fbff9b5c3388c9f418
niccoloruberti <nikiruberti@gmail.com>
2023-05-27 18:27:26 +0200

    completato bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -5,52 +5,58 @@ const grid = document.getElementById('grid');
 const bottone = document.getElementById('generator');
 
 //creo la funzione che mi permette di creare le celle
-function createCell() {
+function createCell(level) {
     let cell = document.createElement('div');
-    //assegno una classe per stilizzare la cella
-    cell.classList.add('cell');
+    //assegno una classe per stilizzare la cella in base alla difficoltà
+    switch (level) {
+        case '1':
+            cell.classList.add('cell-1');
+            break;
+        case '2':
+            cell.classList.add('cell-2');
+            break;
+        case '3':
+            cell.classList.add('cell-3');
+            break;           
+    }
     return cell;
+}
+
+//creo la funzione che mi permette di creare le griglie
+function createGrid(counter, level) {
+    for (let i = 1; i <= counter; i++) {
+        //creo una nuova cella
+        let cell = createCell(level);
+        //inserisco il testo nella cella
+        cell.innerText = i;
+        //creo un evento relativo al click sulla singola cella che gli fa cambiare colore
+        cell.addEventListener('click', function() {
+            this.classList.toggle('active');
+        })
+        //inserisco la nuova cella nella griglia
+        grid.append(cell);
+    }
 }
 
 //creo il ciclo che aggiunge le celle alla griglia quado viene premuto il bottone
 bottone.addEventListener('click', function() {
     let level = document.getElementById('levelSelection').value;
-    console.log(level);
+    let counter;
     switch (level) {
         case '1':
             //ciclo che crea 100 celle
-            for (let counter = 1; counter <= 100; counter++) {
-                console.log(counter)
-                //creo una nuova cella
-                let cell = createCell();
-                //inserisco il testo nella cella
-                cell.innerText = counter;
-                //creo un evento relativo al click sulla singola cella che gli fa cambiare colore
-                cell.addEventListener('click', function() {
-                    this.classList.toggle('active');
-                    console.log(this.innerText);
-                })
-                //inserisco la nuova cella nella griglia
-                grid.append(cell);
-            }
+            counter = 100;
+            createGrid(counter, level);
             break;
         case '2':
             //ciclo che crea 81 celle
-            for (let counter = 1; counter <= 81; counter++) {
-                console.log(counter)
-                //creo una nuova cella
-                let cell = createCell();
-                //inserisco il testo nella cella
-                cell.innerText = counter;
-                //creo un evento relativo al click sulla singola cella che gli fa cambiare colore
-                cell.addEventListener('click', function() {
-                    this.classList.toggle('active');
-                    console.log(this.innerText);
-                })
-                //inserisco la nuova cella nella griglia
-                grid.append(cell);
-            }
+            counter = 81;
+            createGrid(counter, level);
+            break;
+        case '3':
+            //ciclo che crea 49 celle
+            counter = 49;
+            createGrid(counter, level);
+            break;
     }
 })
-
-
